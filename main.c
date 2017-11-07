b25d5e57a81d4e217ae9792d48edee9fd949e25c
Osric Wilkinson <osric@fluffypeople.com>
2017-11-07 21:01:41 +0000

    Basically working

--- main.c
+++ main.c
@@ -6,48 +6,76 @@
  * Created on 07 November 2017, 06:50
  */
 #define _GNU_SOURCE
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define AUTH_FAIL 1
+#define TEMP_ERROR 111
+
+#define PORT 15408
+#define LOCALHOST "127.0.0.1"
 
 int create_socket() {
-
     int sock;
-    struct sockaddr_in name;
-    struct hostent *hostinfo;
-    const char *hostname = "127.0.0.1";
+    struct sockaddr_in dest;
 
-    /* Create the socket. */
-    sock = socket(PF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("socket");
-        exit(EXIT_FAILURE);
+    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket");
+        exit(TEMP_ERROR);
     }
-    
-    name.sin_family = AF_INET;
-    name.sin_port = htons(15408);
-    hostinfo = gethostbyname(hostname);
-    if (hostinfo == NULL) {
-        fprintf(stderr, "Unknown host %s.\n", hostname);
-        exit(EXIT_FAILURE);
-    }
-    name.sin_addr = *(struct in_addr *) hostinfo->h_addr;
 
-    if (bind(sock, (struct sockaddr *) &name, sizeof (name)) < 0) {
-        perror("bind");
-        exit(EXIT_FAILURE);
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(PORT);
+    dest.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(dest.sin_zero, '\0', sizeof dest.sin_zero);
+
+    if (connect(sock, (struct sockaddr*) &dest, sizeof (dest)) != 0) {
+        perror("Connect");
+        exit(TEMP_ERROR);
     }
 
     return sock;
+}
 
+void trimNewline(char *str) {
+    str[strcspn(str, "\n")] = 0;  
+}
+
+void readEnvironment(FILE *socket) {
+    char *env;
+    size_t env_len = 0;
+    while (getline(&env, &env_len, socket) != -1) {
+        char *name, *value;
+        
+        trimNewline(env);
+        
+        // Split env on equals.        
+        name = env;        
+        size_t eq = strcspn(env, "=");
+        name[eq] = 0;        
+        value = env + (eq + 1);        
+        
+        if (setenv(name, value, 1) == -1) {
+            perror("Can't set environment");
+            exit(TEMP_ERROR);
+        }
+        
+        free(env);
+        env_len = 0;
+    }
 }
 
 /*
  * 
  */
-int main(int argc, char** argv) {
+int main(int argc, char** argv) {    
     char *user, *passwd, *remoteIP;
 
     FILE *input = fdopen(3, "r");
@@ -55,32 +83,46 @@ int main(int argc, char** argv) {
     size_t len = 0;
     if (getdelim(&user, &len, '\0', input) < 0) {
         perror("Can't read username");
-        exit(EXIT_FAILURE);
+        exit(TEMP_ERROR);
     }
-    
+
     len = 0;
     if (getdelim(&passwd, &len, '\0', input) < 0) {
         perror("Can't read password");
-        exit(EXIT_FAILURE);
+        exit(TEMP_ERROR);
     }
-    
+
     remoteIP = getenv("TCPREMOTEIP");
-    
+
     int socket_fp = create_socket();
     
     FILE *socket = fdopen(socket_fp, "r+");
-    
-    fprintf(socket, "%s\n%s\n%s", user, passwd, remoteIP == NULL ? "unknown" : remoteIP);
-    
-    clearenv();
-    
-    char *env;
-    size_t env_len;
-    while (getdelim(&env, &env_len, '\n', socket) != -1) {
-        putenv(env);
-        env_len = 0;
-    }
 
-    return (EXIT_SUCCESS);
+    // Write to socket
+    fprintf(socket, "%s\n%s\n%s\n", user, passwd, remoteIP == NULL ? "unknown" : remoteIP);
+    fflush(socket);
+    
+    // Check for success    
+    char *result;
+    size_t result_len = 0;
+    if (getline(&result, &result_len, socket) == -1) {
+        perror("getline");
+        exit(TEMP_ERROR);
+    }
+    
+    trimNewline(result);
+    
+    if (strcmp("success", result) == 0) {
+        clearenv();    
+        readEnvironment(socket);
+            
+        if (execl(argv[1], argv[1], NULL) == -1) {
+            perror("execl");
+            exit(TEMP_ERROR);
+        }
+        // end of process
+    } else {
+        return (AUTH_FAIL);
+    }
 }
 
